e5ebb9f070c1da32c3a2fb25aefacc2de024f363
mrbus <mrbus@mail.ru>
2025-03-03 20:35:00 +0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::time::SystemTime;
 use rayon::prelude::*;
-
 use sha256::digest;
+use std::env;
 
 fn main() {
 	if env::args().len() != 5 {
@@ -21,15 +21,21 @@ fn main() {
     let zeros_count = zeros_count.expect("Wrong zeros count");
     let to_find_count = to_find_count.expect("Wrong to find count");
     let mut found_count = 0;
+    let zeros = "0".repeat(zeros_count);
 
     println!("---- BEGIN ----");
     let time_start = SystemTime::now();
 
     (0..10).into_par_iter().for_each(|thrd| {
-        (1_000_000 * thrd .. 1_000_000 * (thrd+1)).into_iter().for_each(|num| {
+        (1_000_000 * thrd .. 1_000_000 * (thrd+1)).into_iter().try_for_each(|num| {
             let dg = digest(num.to_string());
-            if dg.ends_with(&zeros) { println!("{}\t{}", num, dg) };
-        });
+            if dg.ends_with(&zeros) {
+                println!("{}\t{}", num, dg);
+                found_count += 1;
+                if found_count == to_find_count { return ControlFlow::Break(()) };
+            };
+            ControlFlow::Continue(())
+            });
     });
     println!("Time elapsed = {:?}", time_start.elapsed().unwrap());
     println!("---- END ----");
