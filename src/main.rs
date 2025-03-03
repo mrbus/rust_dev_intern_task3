e983c31ce428f9fc0c2234d102d4f46d69afea0e
mrbus <mrbus@mail.ru>
2025-03-03 18:50:58 +0300

    10 threads

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::time::SystemTime;
+use rayon::prelude::*;
+
+use sha256::digest;
+
+fn main() {
+    let zeros_count = 5;
+    let zeros = "0".repeat(zeros_count);
+    let time_start = SystemTime::now();
+
+    (0..10).into_par_iter().for_each(|thrd| {
+        (1_000_000 * thrd .. 1_000_000 * (thrd+1)).into_iter().for_each(|num| {
+            let dg = digest(num.to_string());
+            if dg.ends_with(&zeros) { println!("{}\t{}", num, dg) };
+        });
+    });
+    println!("Time elapsed = {:?}", time_start.elapsed().unwrap());
+}
